157af4fc816a3be48aad91491577993f264e6d18
Guido Classen <guido.classen@swarco.de>
2017-11-23 11:06:10 +0100

    Added pppd peer file for mobile data network for GPRS/UMTS/LTE communication

--- rootfs_overlay/etc/ppp/shell_gprs_dial.sh
+++ rootfs_overlay/etc/ppp/shell_gprs_dial.sh
@@ -1,19 +1,38 @@
 #!/bin/sh
-#*****************************************************************************/
-#*
-#*  @file          shell_gprs_dial.sh
-#*
-#*  Shell script based GPRS/UMTS dial script
-#*
-#*  @version       0.1
-#*  @date
-#*  @author        Guido Classen
-#*                 SWARCO Traffic Systems GmbH
-#*
-#*  Changes:
-#*    2009-08-28 gc: initial version
-#*
-#****************************************************************************/
+#
+# gprs
+#
+# Package mobile-data-connector 
+# Shell-script based GPRS/UMTS/LTE mobile data network dial script
+#
+# Copyright (c) 2009-2017, SWARCO Traffic Systems GmbH
+#                          Guido Classen <clagix@gmail.com>
+# All rights reserved.
+#     
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# 
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Modification History:
+#     2009-08-28 gc: initial version
+#
 
 echo $0 [Version 2017-05-24 15:22:19 gc]
 
