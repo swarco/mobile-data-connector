af806a939cf246f7014ee7ea096a36338092fbca
Guido Classen <guido.classen@swarco.de>
2017-11-29 16:40:06 +0100

    ntpdate: set -p 1 option when no -p option is specified

--- rootfs_overlay/usr/bin/ntp-query.sh
+++ rootfs_overlay/usr/bin/ntp-query.sh
@@ -1,25 +1,40 @@
 #!/bin/sh
-#*****************************************************************************
-#* 
-#*  @file          ntp-query.sh
-#*
-#*  Run NTP timeserver query as background process. 
-#*  The ntpdate utility can run for several minutes until it has gotten
-#*  a valid time in particular on a slow network connection (GPRS), a
-#*  large timeout (-t option) and if many NTP-hosts are specified.
-#*
-#*  @version       1.0 (\$Revision$)
-#*  @author        Guido Classen <br>
-#*                 SWARCO Traffic Systems GmbH
-#* 
-#*  $LastChangedBy$  
-#*  $Date$
-#*  $URL$
-#*
-#*  @par Modification History:
-#*    2007-12-03 gc: initial version
-#*  
-#*****************************************************************************
+#
+# ntp-query.sh
+#
+# Run NTP timeserver query as background process.  The ntpdate utility can run
+# for several minutes until it has gotten a valid time in particular on a slow
+# network connection (GPRS), a large timeout (-t option) and if many NTP-hosts
+# are specified.
+#
+# Copyright (c) 2007-2017, SWARCO Traffic Systems GmbH
+#                          Guido Classen <clagix@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Modification History:
+#     2007-12-03 gc: initial version
+#
 
 NTPDATE=/usr/bin/ntpdate
 HWCLOCK=/sbin/hwclock
@@ -33,7 +48,16 @@ test -f /etc/default/ntpdate || exit 0
 test -n "$NTPSERVERS" || exit 0
 
 . /etc/create_lock.sh
-create_lock ntp-query
+create_lock ntp-
+
+# set -p 1 option when no -p option is specified
+case $NTPOPTIONS in
+    *-p*)
+        ;;
+    *)
+        NTPOPTIONS="-p 1 $NTPOPTIONS"
+        ;;
+esac
 
 logger -t $0 "Running ntpdate to synchronize clock"
 if $NTPDATE $NTPOPTIONS $NTPSERVERS; then
